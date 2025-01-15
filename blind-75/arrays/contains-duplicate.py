ea277dbe294a2e6572d8c8abf65f56257d4751ce
Nosang Subba <nosangdev@gmail.com>
2025-01-15 14:04:45 +0530

    bucket sort in top-k-els

--- blind-75/arrays/contains-duplicate.py
+++ blind-75/arrays/contains-duplicate.py
@@ -11,4 +11,14 @@ def hasDuplicate(nums: list[int]):
     print(hashMap)
     return False
     
-print(hasDuplicate([1, 2,2, 3]))
+# print(hasDuplicate([1, 2,2, 3]))
+
+def containsDuplicate(nums: list[int]):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+
+print(containsDuplicate([1, 2,2, 3]))
+print(containsDuplicate([1, 2, 3, 4]))
--- blind-75/arrays/top-k-frequent-els.py
+++ blind-75/arrays/top-k-frequent-els.py
@@ -1,11 +1,47 @@
 def topKels(nums: list[int], k: int):
-    hashMap = {}
+    # count = {}
     
-    for num in nums:
-        hashMap[num] = hashMap.get(num, 0) + 1
-    sortedFreq = sorted(hashMap.items(), key=lambda x:x[1], reverse=True)
+    # for num in nums:
+    #     count[num] = count.get(num, 0) + 1
     
-    print([item[0] for item in sortedFreq[:k]])
+    # arr = []
+    # for num, cnt in count.items():
+    #     arr.append([num, cnt])
+    
+    # arr.sort()
+    
+    # result = []
+    
+    # print(arr)
+    # while len(result) < k:
+    #     result.append(arr.pop()[1])
+    
+    # return result
+    
+    # with bucket sort
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+    
+    for n in nums:
+        count[n] = count.get(n, 0) + 1
+        
+    for num, cnt in count.items():
+        freq[cnt].append(num)
+    
+    result = []
+    
+    for i in range(len(freq) - 1, 0, -1):
+        print(i)
+        for n in freq[i]:
+            result.append(n)
+        
+        if len(result) == k:
+            return result
+    
+    return result
     
 
-topKels([1,1,1,2,2,3], 2)
+result = topKels([2,2,3,1,1,1], 2)
+result2 = topKels([1], 1)
+print(result)
+print(result2)

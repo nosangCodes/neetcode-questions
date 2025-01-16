733ecb43e15ddeedf1327307360fdb5e317942f7
Nosang Subba <nosangdev@gmail.com>
2025-01-16 15:12:21 +0530

    bf-sols

--- blind-75/arrays/longest-consec-seq.py
+++ blind-75/arrays/longest-consec-seq.py
@@ -17,5 +17,6 @@ def longestConsecSeq(nums: list[int]) -> int:
     return longestStreak
 
 
-print(longestConsecSeq([0,3,7,2,5,8,4,6,0,1]))
-print(longestConsecSeq([100,4,200,1,3,2]))
+# print(longestConsecSeq([0,3,7,2,5,8,4,6,0,1]))
+# print(longestConsecSeq([100,4,200,1,3,2]))
+
--- blind-75/arrays/product-of-array-except-itself.py
+++ blind-75/arrays/product-of-array-except-itself.py
@@ -9,6 +9,7 @@ def productExceptSelf(nums: list[int]) -> list[int]:
         prefixProduct*=num
         
     suffixes = [1] * len(nums)
+    
     suffixProduct = 1
     for i in range(len(nums), 0, -1):
         suffixes[i - 1] = suffixProduct
@@ -18,9 +19,25 @@ def productExceptSelf(nums: list[int]) -> list[int]:
     for i in range(len(nums)):
         result.append(prefixes[i] * suffixes[i])
         
-    print(prefixes)
-    print(suffixes)
-        
     return result
 
 print(productExceptSelf([1,2,3,4]))
+
+
+def bfSol(nums: list[int]):
+    result = [0] * len(nums) 
+    
+    for i in range(len(nums)):
+        currentProduct = 1
+        for j in range(len(nums)):
+            if i != j:
+                currentProduct *= nums[j]
+        result[i] = currentProduct
+        
+    return result
+
+
+print(bfSol([1, 2, 3, 4]))
+print(bfSol([-1,1,0,-3,3]))
+print(productExceptSelf([-1,1,0,-3,3]))
+    
